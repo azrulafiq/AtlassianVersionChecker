cfb3a45d6226534b741f4262b7d9a5ff3a3555c7
azrulafiq <azrulafiq.w@gmail.com>
2021-09-17 15:34:56 +0800

    added unique variable names

--- run/main.py
+++ run/main.py
@@ -1,4 +1,5 @@
 from sub import *
+import json
 
 # URL for atllasian product
 jira_url = 'https://confluence.atlassian.com/jirasoftware/jira-software-release-notes-776821069.html'
@@ -11,93 +12,136 @@ fisheye_url = 'https://confluence.atlassian.com/fisheye/fisheye-releases-9601557
 eol_url = 'https://confluence.atlassian.com/support/atlassian-support-end-of-life-policy-201851003.html'
 
 # Main data set
-jira_m_data = []
-confl_m_data = []
-bamboo_m_data = []
-bitbucket_m_data = []
-jsm_m_data = []
-crowd_m_data = []
-fisheye_m_data = []
+jira_m_data = [] #done
+confl_m_data = [] #done
+bamboo_m_data = [] #done
+bitbucket_m_data = [] #done
+jsm_m_data = [] #done, need minor adjustment on EOL data parse as the date information is incosistence
+crowd_m_data = [] #done
+fisheye_m_data = [] #done
 
 # Parsing html
 confl_soup = parse_html(confl_url)
 jira_soup = parse_html(jira_url)
 bamboo_soup = parse_html(bamboo_url)
 bitbucket_soup = parse_html(bitbucket_url)
-# crowd_soup = parse_html(crowd_url)
-# fisheye_soup = parse_html(fisheye_url)
-# jsm_soup = parse_html(jsm_url)
+crowd_soup = parse_html(crowd_url)
+fisheye_soup = parse_html(fisheye_url)
+jsm_soup = parse_html(jsm_url)
 eol_soup = parse_html(eol_url)
 
 # CONFLUENCE
-
-# confl_data = confl_soup.find_all('h4', {"id": lambda L: L and L.startswith('ConfluenceReleaseNotes')})
-# confl_eol = eol_soup.find('h2', {"id": lambda L: L and L.startswith('AtlassianSupportEndofLifePolicy-Confluence')}).next_sibling.text
-# confl_eol = confl_eol.split(')')
-# version_data = []
-# eol_data = []
-# push_CONFL_V_LTS(confl_data, version_data)
-# push_V_EOL(confl_eol, eol_data)
-# version_data = remove_empty_data(version_data)
-# eol_data = remove_empty_data(eol_data)
-# confl_m_data = merge_all_data(version_data, eol_data, confl_m_data)
-# confl_m_data = remove_duplicates(confl_m_data)
+confl_data = confl_soup.find_all('h4', {"id": lambda L: L and L.startswith('ConfluenceReleaseNotes')})
+confl_eol = eol_soup.find('h2', {"id": lambda L: L and L.startswith('AtlassianSupportEndofLifePolicy-Confluence')}).next_sibling.text
+confl_eol = confl_eol.split(')')
+version_data = []
+eol_data = []
+push_CONFL_V_LTS(confl_data, version_data)
+push_V_EOL(confl_eol, eol_data)
+version_data = remove_empty_data(version_data)
+eol_data = remove_empty_data(eol_data)
+confl_m_data = merge_all_data(version_data, eol_data, confl_m_data, 'confluence')
+confl_m_data = remove_duplicates(confl_m_data)
 
 # JIRA
-
-# jira_data8 = jira_soup.find('h2', {"id": lambda L: L and L.startswith('JiraSoftwarereleasenotes-JiraSoftware7')}).next_sibling
-# jira_data8 = jira_data8.find_all('li')
-# jira_data7 = jira_soup.find('h2', {"id": lambda L: L and L.startswith('JiraSoftwarereleasenotes-JiraSoftware8')}).next_sibling
-# jira_data7 = jira_data7.find_all('li')
-# jira_eol = eol_soup.find('h2', {"id": lambda L: L and L.startswith('AtlassianSupportEndofLifePolicy-JiraSoftware')}).next_sibling.text
-# jira_eol = jira_eol.split(')')
-# version_data = []
-# eol_data = []
-# push_JIRA_V_LTS(jira_data8, version_data)
-# push_JIRA_V_LTS(jira_data7, version_data)
-# push_V_EOL(jira_eol, eol_data)
-# version_data = remove_empty_data(version_data)
-# eol_data = remove_empty_data(eol_data)
-# jira_m_data = merge_all_data(version_data, eol_data, jira_m_data)
-# jira_m_data = remove_duplicates(jira_m_data)
+jira_data8 = jira_soup.find('h2', {"id": lambda L: L and L.startswith('JiraSoftwarereleasenotes-JiraSoftware7')}).next_sibling
+jira_data8 = jira_data8.find_all('li')
+jira_data7 = jira_soup.find('h2', {"id": lambda L: L and L.startswith('JiraSoftwarereleasenotes-JiraSoftware8')}).next_sibling
+jira_data7 = jira_data7.find_all('li')
+jira_eol = eol_soup.find('h2', {"id": lambda L: L and L.startswith('AtlassianSupportEndofLifePolicy-JiraSoftware')}).next_sibling.text
+jira_eol = jira_eol.split(')')
+version_data = []
+eol_data = []
+push_JIRA_V_LTS(jira_data8, version_data)
+push_JIRA_V_LTS(jira_data7, version_data)
+push_V_EOL(jira_eol, eol_data)
+version_data = remove_empty_data(version_data)
+eol_data = remove_empty_data(eol_data)
+jira_m_data = merge_all_data(version_data, eol_data, jira_m_data, 'jira')
+jira_m_data = remove_duplicates(jira_m_data)
 
 # BAMBOO
-
-# bamboo_data8 = bamboo_soup.find('h2', {"id": lambda L: L and L.startswith('BambooReleaseNotes-Bamboo8releasenotes')}).next_sibling
-# bamboo_data8 = bamboo_data8.find_all('li')
-# bamboo_data7 = bamboo_soup.find('h2', {"id": lambda L: L and L.startswith('BambooReleaseNotes-Bamboo7releasenotes')}).next_sibling
-# bamboo_data7 = bamboo_data7.find_all('li')
-# bamboo_data6 = bamboo_soup.find('h2', {"id": lambda L: L and L.startswith('BambooReleaseNotes-Bamboo6releasenotes')}).next_sibling
-# bamboo_data6 = bamboo_data6.find_all('li')
-# bamboo_eol = eol_soup.find('h2', {"id": lambda L: L and L.startswith('AtlassianSupportEndofLifePolicy-Bamboo')}).next_sibling.text
-# bamboo_eol = bamboo_eol.split(')')
-# version_data = []
-# eol_data = []
-# push_BAMBOO_V_LTS(bamboo_data8, version_data)
-# push_BAMBOO_V_LTS(bamboo_data7, version_data)
-# push_BAMBOO_V_LTS(bamboo_data6, version_data)
-# push_V_EOL(bamboo_eol, eol_data)
-# version_data = remove_empty_data(version_data)
-# eol_data = remove_empty_data(eol_data)
-# bamboo_m_data = merge_all_data(version_data, eol_data, bamboo_m_data)
-# bamboo_m_data = remove_duplicates(bamboo_m_data)
+bamboo_data8 = bamboo_soup.find('h2', {"id": lambda L: L and L.startswith('BambooReleaseNotes-Bamboo8releasenotes')}).next_sibling
+bamboo_data8 = bamboo_data8.find_all('li')
+bamboo_data7 = bamboo_soup.find('h2', {"id": lambda L: L and L.startswith('BambooReleaseNotes-Bamboo7releasenotes')}).next_sibling
+bamboo_data7 = bamboo_data7.find_all('li')
+bamboo_data6 = bamboo_soup.find('h2', {"id": lambda L: L and L.startswith('BambooReleaseNotes-Bamboo6releasenotes')}).next_sibling
+bamboo_data6 = bamboo_data6.find_all('li')
+bamboo_eol = eol_soup.find('h2', {"id": lambda L: L and L.startswith('AtlassianSupportEndofLifePolicy-Bamboo')}).next_sibling.text
+bamboo_eol = bamboo_eol.split(')')
+version_data = []
+eol_data = []
+push_BAMBOO_V_LTS(bamboo_data8, version_data)
+push_BAMBOO_V_LTS(bamboo_data7, version_data)
+push_BAMBOO_V_LTS(bamboo_data6, version_data)
+push_V_EOL(bamboo_eol, eol_data)
+version_data = remove_empty_data(version_data)
+eol_data = remove_empty_data(eol_data)
+bamboo_m_data = merge_all_data(version_data, eol_data, bamboo_m_data, 'bamboo')
+bamboo_m_data = remove_duplicates(bamboo_m_data)
 
 # BITBUCKET
-
 bitbucket_data = bitbucket_soup.find_all('h2', {"id": lambda L: L and L.startswith('Releasenotes-Bitbucket')})
 bitbucket_eol = eol_soup.find('h2', {"id": lambda L: L and L.startswith('AtlassianSupportEndofLifePolicy-Bamboo')}).next_sibling.text
 bitbucket_eol = bitbucket_eol.split(')')
-
 version_data = []
 eol_data = []
-
 push_BIT_V_LTS(bitbucket_data, version_data)
 push_V_EOL(bitbucket_eol, eol_data)
-
 version_data = remove_empty_data(version_data)
 eol_data = remove_empty_data(eol_data)
-bitbucket_m_data = merge_all_data(version_data, eol_data, bitbucket_m_data)
+bitbucket_m_data = merge_all_data(version_data, eol_data, bitbucket_m_data, 'bitbucket')
 bitbucket_m_data = remove_duplicates(bitbucket_m_data)
 
-for a in bitbucket_m_data:
+# Crowd
+crowd_data = crowd_soup.find_all('td', {"class": lambda L: L and L.startswith('highlight-grey confluenceTd')})
+crowd_eol = eol_soup.find('h2', {"id": lambda L: L and L.startswith('AtlassianSupportEndofLifePolicy-Crowd')}).next_sibling.text
+crowd_eol = crowd_eol.split(')')
+version_data = []
+eol_data = []
+push_CROWD_V_LTS(crowd_data, version_data)
+push_V_EOL(crowd_eol, eol_data)
+version_data = remove_empty_data(version_data)
+eol_data = remove_empty_data(eol_data)
+crowd_m_data = merge_all_data(version_data, eol_data, crowd_m_data, 'crowd')
+crowd_m_data = remove_duplicates(crowd_m_data)
+
+# JSM (need adjustment on date parser)
+# jsm_data = jsm_soup.find_all('h4', {"id": lambda L: L and L.startswith('JiraServiceManagementReleaseNotes-JiraServiceManagement')})
+# jsd_data = jsm_soup.find_all('h4', {"id": lambda L: L and L.startswith('JiraServiceManagementReleaseNotes-JiraServiceDesk')})
+# jsm_eol = eol_soup.find('h2', {"id": lambda L: L and L.startswith('AtlassianSupportEndofLifePolicy-JiraServiceManagement')}).next_sibling.next_sibling.text
+# jsm_eol = jsm_eol.split(')')
+# jsm_data = jsm_data + jsd_data
+# version_data = []
+# eol_data = []
+# push_JSM_V_LTS(jsm_data, version_data)
+# push_V_EOL(jsm_eol, eol_data)
+# version_data = remove_empty_data(version_data)
+# eol_data = remove_empty_data(eol_data)
+# jsm_m_data = merge_all_data(version_data, eol_data, jsm_m_data)
+# jsm_m_data = remove_duplicates(jsm_m_data)
+
+# fish-eye
+fisheye_data = fisheye_soup.find_all('td', {"class": lambda L: L and L.startswith('highlight-grey confluenceTd')})
+fisheye_eol = eol_soup.find('h2', {"id": lambda L: L and L.startswith('AtlassianSupportEndofLifePolicy-Fisheye/Crucible')}).next_sibling.next_sibling.text
+fisheye_eol = fisheye_eol.split(')')
+version_data = []
+eol_data = []
+push_FISH_V_LTS(fisheye_data, version_data)
+push_V_EOL(fisheye_eol, eol_data)
+version_data = remove_empty_data(version_data)
+eol_data = remove_empty_data(eol_data)
+fisheye_m_data = merge_all_data(version_data, eol_data, fisheye_m_data, 'fisheye')
+fisheye_m_data = remove_duplicates(fisheye_m_data)
+
+# Combine all list
+mergeall = confl_m_data + jira_m_data + bitbucket_m_data + crowd_m_data + fisheye_m_data + bamboo_m_data + jsm_m_data
+
+# Output to json file
+
+# with open("atlassian_product_version.json", "w") as writeJSON:
+#     json.dump(mergeall, writeJSON, indent=4)
+
+# Standard output
+for a in mergeall:
     print(a)
--- run/sub.py
+++ run/sub.py
@@ -1,5 +1,6 @@
 import re
 import requests
+from unicodedata import normalize
 from bs4 import BeautifulSoup
 from datetime import datetime
 
@@ -10,40 +11,39 @@ def parse_html(url):
 
 def push_CONFL_V_LTS(collection_data, maindata):
     for confl in collection_data:
-        item = {}
         lts = 'LONG TERM SUPPORT'
-
+        item = {}
         item['version'] = confl.text.replace('Confluence ', '')
 
         if lts in item['version']:
             item['long term support'] = True
             item['version'] = item['version'].replace(lts, '')
-            item['version'] = float(item['version'])
+            item['version'] = normalize("NFKD", item['version'])
         else:
             item['long term support'] = False
-            item['version'] = float(item['version'])
+            item['version'] = normalize("NFKD", item['version'])
         maindata.append(item)
 
 def push_BIT_V_LTS(collection_data, maindata):
-    for confl in collection_data:
+    for bit in collection_data:
         item = {}
         lts = ' (Long Term Support release)'
-        confl = confl.text
+        bit = bit.text
 
-        confl = confl.replace('Bitbucket Data Center and Server ', '')
-        confl = confl.replace('Bitbucket Server and Data Center ', '')
-        confl = confl.replace('Bitbucket Server ', '')
-        item['version'] = confl
+        bit = bit.replace('Bitbucket Data Center and Server ', '')
+        bit = bit.replace('Bitbucket Server and Data Center ', '')
+        bit = bit.replace('Bitbucket Server ', '')
+        item['version'] = bit
 
         # print('Version: ' + str(item['version']))
 
         if lts in item['version']:
             item['long term support'] = True
             item['version'] = item['version'].replace(lts, '')
-            item['version'] = float(item['version'])
+            # item['version'] = float(item['version'])
         else:
             item['long term support'] = False
-            item['version'] = float(item['version'])
+            # item['version'] = float(item['version'])
         maindata.append(item)
 
 def push_JIRA_V_LTS(collection_data, maindata):
@@ -56,17 +56,36 @@ def push_JIRA_V_LTS(collection_data, maindata):
 
         version_data = re.findall('\d+\.\d+', str(nonlts))
         for version in version_data:
-            item['version'] = float(version)
+            item['version'] = version
             item['long term support'] = False
             # print('Version: ' + str(item['version']))
             # print('LTS: ' + str(item['long term support']))
 
         version_data = re.findall('\d+\.\d+', str(lts))
         for version in version_data:
-            item['version'] = float(version)
+            item['version'] = version
             item['long term support'] = True
             # print('Version: ' + str(item['version']))
             # print('LTS: ' + str(item['long term support']))
+
+        maindata.append(item)
+
+def push_JSM_V_LTS(collection_data, maindata):
+    for data in collection_data:
+        item = {}
+        set_data = data.text
+        jsm = re.findall('Jira Service Management\D\d.\d+', set_data)
+        jsd = re.findall('Jira Service Desk\D\d.\d+', set_data)
+
+        version_data = re.findall('\d+\.\d+', str(jsm))
+        for version in version_data:
+            item['version'] = version
+            item['long term support'] = False
+
+        version_data = re.findall('\d+\.\d+', str(jsd))
+        for version in version_data:
+            item['version'] = version
+            item['long term support'] = False
 
         maindata.append(item)
 
@@ -79,11 +98,23 @@ def push_BAMBOO_V_LTS(collection_data, maindata):
 
         version_data = re.findall('\d+\.\d+', str(nonlts))
         for version in version_data:
-            item['version'] = f"{version:.2f}"
+            item['version'] = version
             item['long term support'] = False
-            # print('Version: ' + str(item['version']))
-            # print('LTS: ' + str(item['long term support']))
 
+        maindata.append(item)
+
+def push_CROWD_V_LTS(collection_data, maindata):
+    for data in collection_data:
+        item = {}
+        item['version'] = data.text
+        item['long term support'] = False
+        maindata.append(item)
+
+def push_FISH_V_LTS(collection_data, maindata):
+    for data in collection_data:
+        item = {}
+        item['version'] = data.text
+        item['long term support'] = False
         maindata.append(item)
 
 def push_V_EOL(collection_data, maindata):
@@ -91,6 +122,7 @@ def push_V_EOL(collection_data, maindata):
         item = {}
         set_data = set_data.replace('(', '')
         set_data = set_data.replace(' ', '')
+        set_data = set_data.replace('EOLdateextendedto', '')
         # To match with datetime library
         set_data = set_data.replace('January', 'Jan')
         set_data = set_data.replace('February', 'Feb')
@@ -108,7 +140,8 @@ def push_V_EOL(collection_data, maindata):
         # Massage version data
         newSet = re.findall('\d+\.\d+', new_data)
         for version in newSet:
-            item['version'] = float(version)
+            # item['version'] = float(version)
+            item['version'] = version
 
         # Massage EOL data
         # Special Case 1 (Conflunce)
@@ -136,16 +169,19 @@ def remove_empty_data(data):
     newSet = list(filter(None, data))
     return newSet
 
-def merge_all_data(set1, set2, finaldata):
+def merge_all_data(set1, set2, finaldata, product):
     for a in set1:
         item = {}
-        item['version'] = a['version']
-        item['long term support'] = a['long term support']
-        item['eol'] = 'null' # No data from official site
+        # item['product'] = product
+        # item['version'] = a['version']
+        item[product + '_version'] = a['version']
+        item[product + '_long_term_support'] = a['long term support']
+        item[product + '_end_of_life'] = 'null' # No data from official site
         for b in set2:
             b['version']
             if b['version'] == a['version']:
-                item['eol'] = b['eol']
+                # item['eol'] = b['eol']
+                item[product + '_end_of_life'] = b['eol']
         finaldata.append(item)
     return finaldata
 
